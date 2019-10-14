dd6e4a5b6ef34d258f68e3aa1ee06eee22ae4fda
Sasha Sushko <sushko@outlook.com>
2019-10-14 20:47:34 +0300

    Простой вариант:
    - положить разметку сразу в папку назначения
    - добавить несколько точек входа
    - настроить точку выхода
    - связать страницы простой относительной ссылкой

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        // entry может быть не просто строкой, а объектом.
+        // Где каждый ключ - имя точки входа. Таким образом
+        // можно сделать по одной точке входа на кажую страницу
+        index: './src/index.js',
+        about: './src/about.js',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        // строка в filename поддерживает шаблоны, задаются в квадратных скобках,
+        // вместо которых подставляется какое-то значение.
+        // Подробнее можно на сайте Вебпака почитать - https://webpack.js.org/configuration/output/
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:  [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // здесь тоже поддерживаются шаблоны в строке
+            filename: '[name].bundle.css'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        open: true,
     }
 };
